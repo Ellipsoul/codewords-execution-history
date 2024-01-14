17d12aaa54b20d061935b9d4e65aa6c302d55c5b
Aron Teh <aronteh.chess@gmail.com>
2024-01-14 00:04:39 +0000

    Add loading state to home page

--- app/page.tsx
+++ app/page.tsx
@@ -4,10 +4,12 @@ import React, { useEffect, useState } from "react";
 
 export default function HomePage(): JSX.Element {
   const [appData, setAppData] = useState<any[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
 
   // Fetch mocked app data
   useEffect(() => {
     async function fetchData() {
+      setIsLoading(true);
       const [response1, response2] = await Promise.all([
         fetch("/mock_app_data/app_1.json"),
         fetch("/mock_app_data/app_2.json"),
@@ -20,6 +22,7 @@ export default function HomePage(): JSX.Element {
       console.log(data1);
 
       setAppData([data1, data2]);
+      setIsLoading(false);
     }
 
     try {
@@ -29,5 +32,5 @@ export default function HomePage(): JSX.Element {
     }
   }, []);
 
-  return <div>{appData[0]["app_id"]}</div>;
+  return isLoading ? <div>Loading...</div> : <div>{appData[0]["app_id"]}</div>;
 }
