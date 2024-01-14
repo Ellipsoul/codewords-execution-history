441dc406c9a5058898acbc589e4f057ff54e6378
Aron Teh <aronteh.chess@gmail.com>
2024-01-14 21:03:03 +0000

    Create dynamically loaded accordion cards

--- app/app/executions/[app_id]/page.tsx
+++ app/app/executions/[app_id]/page.tsx
@@ -9,38 +9,46 @@ import {
   AccordionTrigger,
 } from "@/components/ui/accordion";
 
+const NUM_EXECUTIONS_PER_FETCH = 5;
+
 export default function ExecutionsPage({
   params,
 }: {
   params: { app_id: string };
 }) {
   const [appName, setAppName] = useState<string>("");
-  const [appExecutions, setAppExecutions] = useState<any[]>([]);
+  const [appExecutionIds, setAppExecutionsIds] = useState<number[]>([]);
   const [isLoading, setIsLoading] = useState(true);
 
   const [searchTerm, setSearchTerm] = useState<string>("");
+  const [itemsToDisplay, setItemsToDisplay] = useState<number>(
+    NUM_EXECUTIONS_PER_FETCH
+  );
 
   // Load mocked app data and executions into component
   useEffect(() => {
-    async function fetchData() {
+    async function fetchAppData() {
       setIsLoading(true);
 
       const appDataUrl = `/mock_app_data/app_${params.app_id}.json`;
       const appDataResponse = await fetch(appDataUrl);
       const appData = await appDataResponse.json();
 
-      // const appExecutionsUrl = `/mock_app_data/app_${params.app_id}_executions.json`;
+      const appExecutionsIdsUrl = `/mock_app_data/executions_${params.app_id}/execution_ids.json`;
+      const appExecutionsIdsResponse = await fetch(appExecutionsIdsUrl);
+      const appExecutionsIds = await appExecutionsIdsResponse.json();
 
       setAppName(
         appData["workflow_description_component"]["workflow_description"][
           "name"
         ]
       );
+      setAppExecutionsIds(appExecutionsIds["execution_ids"]);
       setIsLoading(false);
     }
 
     try {
-      fetchData();
+      fetchAppData();
     } catch (error) {
       console.error("Error fetching JSON data:", error);
     }
@@ -48,7 +56,10 @@ export default function ExecutionsPage({
 
   // Load in more executions
   const showMoreExecutions = () => {
-    console.log("show more executions");
+    setItemsToDisplay(
+      (prevNumItemsToDisplay) =>
+        prevNumItemsToDisplay + NUM_EXECUTIONS_PER_FETCH
+    );
   };
 
   return isLoading ? (
@@ -75,8 +86,15 @@ export default function ExecutionsPage({
           onChange={(e) => setSearchTerm(e.target.value)}
         />
       </section>
-      <ExecutionsAccordion />
-      <button className="font-bold text-sm" onClick={showMoreExecutions}>
+      <ExecutionsAccordion
+        appExecutionIds={appExecutionIds}
+        itemsToDisplay={itemsToDisplay}
+      />
+      <button
+        className="font-bold text-sm disabled:opacity-20 disabled:cursor-not-allowed"
+        onClick={showMoreExecutions}
+        disabled={itemsToDisplay >= appExecutionIds.length}
+      >
         View More
       </button>
     </main>
@@ -84,43 +102,30 @@ export default function ExecutionsPage({
 }
 
 // Accordion listing the app executions
-function ExecutionsAccordion(): JSX.Element {
+function ExecutionsAccordion({
+  appExecutionIds,
+  itemsToDisplay,
+}: {
+  appExecutionIds: number[];
+  itemsToDisplay: number;
+}): JSX.Element {
   return (
     <Accordion
       type="single"
       collapsible
       className="w-full flex flex-col gap-y-2 pb-3"
     >
-      <AccordionItem
-        value="item-1"
-        className="rounded-sm bg-zinc-200 hover:bg-zinc-300 dark:bg-zinc-800 dark:hover:bg-zinc-700
+      {appExecutionIds.slice(0, itemsToDisplay).map((executionId) => (
+        <AccordionItem
+          key={executionId}
+          value={String(executionId)}
+          className="rounded-sm bg-zinc-200 hover:bg-zinc-300 dark:bg-zinc-800 dark:hover:bg-zinc-700
         px-4 cursor-pointer border-zinc-300 dark:border-zinc-700"
-      >
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem
-        value="item-2"
-        className="rounded-sm bg-zinc-200 hover:bg-zinc-300 dark:bg-zinc-800 dark:hover:bg-zinc-700
-        px-4 cursor-pointer border-zinc-300 dark:border-zinc-700"
-      >
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
-      <AccordionItem
-        value="item-3"
-        className="rounded-sm bg-zinc-200 hover:bg-zinc-300 dark:bg-zinc-800 dark:hover:bg-zinc-700
-        px-4 cursor-pointer border-zinc-300 dark:border-zinc-700"
-      >
-        <AccordionTrigger>Is it accessible?</AccordionTrigger>
-        <AccordionContent>
-          Yes. It adheres to the WAI-ARIA design pattern.
-        </AccordionContent>
-      </AccordionItem>
+        >
+          <AccordionTrigger>Execution {executionId}</AccordionTrigger>
+          <AccordionContent>Test</AccordionContent>
+        </AccordionItem>
+      ))}
     </Accordion>
   );
 }
