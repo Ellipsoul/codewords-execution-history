c1bdc772bcef9c0c2280c44ad520fdef1079ef86
Aron Teh <aronteh.chess@gmail.com>
2024-01-14 13:57:07 +0000

    Access app id dynamically based on route in the executions page

--- app/app/executions/[app_id]/page.tsx
+++ app/app/executions/[app_id]/page.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 
-export default function ExecutionsPage() {
-  return <div>Executions Page</div>;
+export default function ExecutionsPage({
+  params,
+}: {
+  params: { app_id: string };
+}) {
+  return <div>This is the executions page for app_id: {params.app_id}</div>;
 }
